499f5d4daf15ebff23331b392482d09049f73656
Pierr Grimaldo Vidalon <47004333+pgrimaldo@users.noreply.github.com>
2020-03-24 01:46:29 -0500

    actualizado UBIGEO

--- app/Http/Controllers/UbigeoController.php
+++ app/Http/Controllers/UbigeoController.php
@@ -18,36 +18,26 @@ use App\Http\Repositories\UbigeoRepository;
      */
 
     public function getDeparment()
-    {        
+    {
           $oUbigeo = new UbigeoRepository();
-          $n = $oUbigeo->getDeparment();                 
+          $n = $oUbigeo->getDeparment();
         return response()->json($n);
     }
 
     public function getProvince(Request $request)
-    {        
-        $oUbigeo = new UbigeoRepository();        
-        $n = $oUbigeo->getProvince($request->id);        
+    {
+        $oUbigeo = new UbigeoRepository();
+        $n = $oUbigeo->getProvince($request->id);
         return response()->json($n);
     }
 
     public function getDistrict(Request $request)
-    {        
+    {
         $oUbigeo = new UbigeoRepository();
-        $n = $oUbigeo->getDistrict($request->idprovince, $request->iddepartment);        
+        $n = $oUbigeo->getDistrict($request->idprovince, $request->iddepartment);
         return response()->json($n);
     }
-	
-	/*
-		N° : REQ_PIERR_1
-		GESTION: PROVEEDOR
-		AUTOR: PIERR GRIMALDO
-		FECHA ACTUALIZADO: 2020-03-23
-		FUNCION: ´ListarDistritoProveedor´ Listará el distrito de la vista proveedor.
-				 ´ListarTipoServicio´ Listará el tipo de proveedor en la vista proveedor.
-	*/
-	
-	
+
     public function ListarDistritoProveedor(Request $request)
     {
         $oUbigeo = new UbigeoRepository();
--- app/Http/Repositories/UbigeoRepository.php
+++ app/Http/Repositories/UbigeoRepository.php
@@ -4,7 +4,7 @@ namespace App\Http\Repositories;
 
 
 use Illuminate\Support\Facades\DB;
- class UbigeoRepository 
+ class UbigeoRepository
 {
     /**
      * Show the profile for the given user.
@@ -12,33 +12,45 @@ use Illuminate\Support\Facades\DB;
      * @param  int  $id
      * @return View
      */
+
+
+    #region Funciones publicas
     public function getDeparment()
     {
-        $departments = DB::select('select id,nombre from Departamento');
-        return $departments;
+        try{
+            $departments = DB::select('CALL sp_S_ListarDepartamento()');
+            return $departments;
+        }
+        catch (MySQLDuplicateKeyException $e) {
+            $e->getMessage();
+        }
+        catch (MySQLException $e) {
+            $e->getMessage();
+        }
+        catch (Exception $e) {
+            $e->getMessage();
+        }
     }
 
     public function getProvince($iddepartment)
     {
-        $provinces = DB::select('select id,nombre from Provincia where iddepartamento = ?',[$iddepartment]);
-        return $provinces;
+        try {
+            $provinces = DB::select('CALL sp_S_ListarPronvinciaProveedor(?)',[$iddepartment]);
+            return $provinces;
+        }
+        catch (MySQLDuplicateKeyException $e) {
+            $e->getMessage();
+        }
+        catch (MySQLException $e) {
+            $e->getMessage();
+        }
+        catch (Exception $e) {
+            $e->getMessage();
+        }
+
     }
 
-    public function getDistrict($idprovince, $iddepartment)
-    {
-        $districts = DB::select('select id,nombre from Distrito where idprovincia = ? and iddepartamento = ?', [ $idprovince, $iddepartment]);
-        return $districts;
-    }
-	
-	/*
-		N° : REQ_PIERR_1
-		GESTION: PROVEEDOR
-		AUTOR: PIERR GRIMALDO
-		FECHA ACTUALIZADO: 2020-03-23
-		FUNCION: ´ListarDistritoProveedor´ Listará el distrito de la vista proveedor.
-				 ´ListarTipoServicio´ Listará el tipo de proveedor en la vista proveedor.
-	*/
-	
+
 
     public function ListarDistritoProveedor($idprovince, $iddepartment )
     {
@@ -73,5 +85,8 @@ use Illuminate\Support\Facades\DB;
             $e->getMessage();
         }
     }
+
+
+    #endregion
 
 }
